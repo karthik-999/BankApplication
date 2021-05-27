ed22d0940ddcffda9882cc3fd55f1c206f8565be
karthik-999 <karthik_pushpala@outlook.com>
2021-05-27 16:52:25 +0530

    Added Exceptions and UUID

--- src/main/java/com/bankapplication/services/AccountServiceImpl.java
+++ src/main/java/com/bankapplication/services/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.bankapplication.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
@@ -23,10 +24,10 @@ public class AccountServiceImpl implements IAccountService {
 
 	@Autowired
 	AccountRepository accountRepository;
-	
+
 	@Autowired
 	ModelMapper modelMapper;
-	
+
 	@Autowired
 	AccountUtilities accountUtilities;
 
@@ -41,18 +42,26 @@ public class AccountServiceImpl implements IAccountService {
 	}
 
 	@Override
-	public AccountDetailsResponseDTO saveAccount(@Valid AccountDetailsDTO accountDTO ) {
+	public AccountDetailsResponseDTO saveAccount(@Valid AccountDetailsDTO accountDTO) {
 		Account account = new Account();
 		AccountDetailsResponseDTO accountDetailsResponseDTO = new AccountDetailsResponseDTO();
 		account = accountUtilities.copyPropertiesFromDTOToEntity(accountDTO, account);
+		if (account != null && account.getAccountNumber() == null) {
+			account.setAccountNumber(UUID.randomUUID().randomUUID().toString().substring(0, 9));
+		}
+		if (account.getBeneficiaryAccounts() != null) {
+			for (Beneficiery beneficiaryAccount : account.getBeneficiaryAccounts()) {
+				beneficiaryAccount.setBeneficieryNumber(UUID.randomUUID().toString().substring(9, 19));
+			}
+		}
 		account = accountRepository.save(account);
 		accountDetailsResponseDTO = accountUtilities.copyPropertiesFromEntityToDTO(account);
 		return accountDetailsResponseDTO;
-		
+
 	}
 
 	@Override
-	public List<AccountDetailsResponseDTO> getAllAccounts(){
+	public List<AccountDetailsResponseDTO> getAllAccounts() {
 		List<AccountDetailsResponseDTO> accountResponse = new ArrayList<AccountDetailsResponseDTO>();
 		List<Account> accounts = accountRepository.findAll();
 		for (Account account : accounts) {
@@ -61,14 +70,13 @@ public class AccountServiceImpl implements IAccountService {
 		return accountResponse;
 	}
 
-	
 	@Override
 	public void deleteAccount(Long accountId) {
 		Optional<Account> account = accountRepository.findByAccountId(accountId);
 		if (account.isPresent()) {
 			accountRepository.delete(account.get());
 		}
-		
+
 	}
 
 	@Override
@@ -79,7 +87,7 @@ public class AccountServiceImpl implements IAccountService {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public Account saveAccount(Account account) {
 		return accountRepository.save(account);
