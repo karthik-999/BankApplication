4f77f5360604faa2a631784318aadd680cc1fb91
karthik-999 <karthik_pushpala@outlook.com>
2021-06-06 22:58:21 +0530

    updates

--- src/main/java/com/bankapplication/services/AccountServiceImpl.java
+++ src/main/java/com/bankapplication/services/AccountServiceImpl.java
@@ -30,163 +30,161 @@ import com.bankapplication.services.interfaces.ITransactionService;
 @Service
 public class AccountServiceImpl implements IAccountService {
 
-	@Autowired
-	AccountRepository accountRepository;
+  @Autowired
+  public ITransactionService transactionService;
 
-	@Autowired
-	public ITransactionService transactionService;
+  @Autowired
+  AccountRepository accountRepository;
 
-	@Override
-	public Account getByAccount(@Valid Long accountId) {
+  @Override
+  public Account getByAccount(@Valid Long accountId) {
 
-		Optional<Account> account = accountRepository.findById(accountId);
-		if (account.isPresent())
-			return account.get();
-		else
-			return null;
-	}
+    Optional<Account> account = accountRepository.findById(accountId);
+    if (account.isPresent()) {
+      return account.get();
+    } else {
+      return new Account();
+    }
+  }
 
-	@Override
-	public AccountDetailsResponseDTO saveAccount(@Valid AccountDetailsDTO accountDTO) {
-		var account = new Account();
-		var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
-		BeanUtils.copyProperties(accountDTO, account);
-		if (account.getAccountNumber() == null) {
-			account.setAccountNumber(UUID.randomUUID().toString().substring(0, 9).toUpperCase());
-			account = accountRepository.save(account);
-		}
-		BeanUtils.copyProperties(account, accountDetailsResponseDTO);
-		return accountDetailsResponseDTO;
+  @Override
+  public AccountDetailsResponseDTO saveAccount(@Valid AccountDetailsDTO accountDTO) {
+    var account = new Account();
+    var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
+    BeanUtils.copyProperties(accountDTO, account);
+    if (account.getAccountNumber() == null) {
+      account.setAccountNumber(UUID.randomUUID().toString().substring(0, 9).toUpperCase());
+      account = accountRepository.save(account);
+    }
+    BeanUtils.copyProperties(account, accountDetailsResponseDTO);
+    return accountDetailsResponseDTO;
+  }
 
-	}
+  @Override
+  public List<AccountDetailsResponseDTO> getAllAccounts(Pageable pageable) {
+    List<AccountDetailsResponseDTO> accountResponse = new ArrayList<>();
+    Page<Account> accountsPage = accountRepository.findAll(pageable);
+    for (Account account : accountsPage.getContent()) {
+      var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
+      BeanUtils.copyProperties(account, accountDetailsResponseDTO);
+      accountResponse.add(accountDetailsResponseDTO);
+    }
+    return accountResponse;
+  }
 
-	@Override
-	public List<AccountDetailsResponseDTO> getAllAccounts(Pageable pageable) {
-		List<AccountDetailsResponseDTO> accountResponse = new ArrayList<>();
-		Page<Account> accountsPage = accountRepository.findAll(pageable);
-		for (Account account : accountsPage.getContent()) {
-			var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
-			BeanUtils.copyProperties(account, accountDetailsResponseDTO);
-			accountResponse.add(accountDetailsResponseDTO);
-		}
-		return accountResponse;
-	}
+  @Override
+  public ResponseMessage deleteAccount(Long accountId) {
+    Optional<Account> accountWrapper = accountRepository.findByAccountId(accountId);
+    if (accountWrapper.isPresent()) {
+      var account = accountWrapper.get();
+      accountRepository.delete(account);
+      return new ResponseMessage("Successfully Deleted");
+    }
+    return new ResponseMessage("Account doesn't Exists - Enter Valid AccountId");
+  }
 
-	@Override
-	public ResponseMessage deleteAccount(Long accountId) {
-		Optional<Account> accountWrapper = accountRepository.findByAccountId(accountId);
-		if (accountWrapper.isPresent()) {
-			var account = accountWrapper.get();
-			accountRepository.delete(account);
-			return new ResponseMessage("Successfully Deleted");
-		}
-		return new ResponseMessage("Account doesn't Exists - Enter Valid AccountId");
-	}
+  @Override
+  public AccountDetailsResponseDTO getAccount(Long accountId) {
+    var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
+    Optional<Account> account = accountRepository.findByAccountId(accountId);
+    if (account.isPresent()) {
+      BeanUtils.copyProperties(account.get(), accountDetailsResponseDTO);
+      return accountDetailsResponseDTO;
+    }
+    return null;
+  }
 
-	@Override
-	public AccountDetailsResponseDTO getAccount(Long accountId) {
-		var accountDetailsResponseDTO = new AccountDetailsResponseDTO();
-		Optional<Account> account = accountRepository.findByAccountId(accountId);
-		if (account.isPresent()) {
-			BeanUtils.copyProperties(account.get(), accountDetailsResponseDTO);
-			return accountDetailsResponseDTO;
-		}
-		return null;
-	}
+  @Override
+  public Account saveAccount(Account account) {
+    return accountRepository.save(account);
+  }
 
-	@Override
-	public Account saveAccount(Account account) {
-		return accountRepository.save(account);
-	}
+  @Override
+  public BeneficiaryDetailsResponseDTO updateAccount(AddBeneficiaryDetailsDTO beneficiaryDetails) {
 
-	@Override
-	public BeneficiaryDetailsResponseDTO updateAccount(AddBeneficiaryDetailsDTO beneficiaryDetails) {
+    var account = new Account();
+    var accountDB = new Account();
 
-		var account = new Account();
-		var accountDB = new Account();
+    var beneficiaryDetailsResponseDTO = new BeneficiaryDetailsResponseDTO();
+    if (beneficiaryDetails != null && beneficiaryDetails.getAccountId() != null) {
+      accountDB = getByAccount(beneficiaryDetails.getAccountId());
+    }
+    if (account.getUser() == null && accountDB != null) {
+      account.setUser(accountDB.getUser());
+    }
+    if (beneficiaryDetails != null) {
+      BeanUtils.copyProperties(beneficiaryDetails, account);
+      account = accountRepository.save(account);
+    } else {
+      account = accountDB;
+    }
 
-		var beneficiaryDetailsResponseDTO = new BeneficiaryDetailsResponseDTO();
-		if (beneficiaryDetails != null && beneficiaryDetails.getAccountId() != null) {
-			accountDB = getByAccount(beneficiaryDetails.getAccountId());
-		}
-		if (account.getUser() == null && accountDB != null) {
-			account.setUser(accountDB.getUser());
-		}
-		if (beneficiaryDetails != null) {
-			BeanUtils.copyProperties(beneficiaryDetails, account);
-			account = accountRepository.save(account);
-		} else {
-			account = accountDB;
-		}
+    BeanUtils.copyProperties(account, beneficiaryDetailsResponseDTO);
+    return beneficiaryDetailsResponseDTO;
+  }
 
-		BeanUtils.copyProperties(account, beneficiaryDetailsResponseDTO);
-		return beneficiaryDetailsResponseDTO;
-	}
+  @Override
+  public Account getByAccount(String beneficieryAccountNumber) {
+    return accountRepository.getByAccountNumber(beneficieryAccountNumber);
+  }
 
-	@Override
-	public Account getByAccount(String beneficieryAccountNumber) {
-		return accountRepository.getByAccountNumber(beneficieryAccountNumber);
-	}
+  public void postAmountTransfer(TransferAccountDetailsDTO requestDetails, Account userAccount, Account beneficieryAccount) {
+    var transaction = new Transaction();
+    transaction.setSenderAccount(userAccount);
+    transaction.setReceiverAccount(beneficieryAccount);
+    transaction.setTransactionNumber(UUID.randomUUID().toString().replace("-", "").substring(12, 21));
+    transaction.setAmount(requestDetails.getAmount().doubleValue());
+    transaction.setCreatedTime(LocalDateTime.now());
+    transactionService.saveTransaction(transaction);
+  }
 
-	public void postAmountTransfer(TransferAccountDetailsDTO requestDetails, Account userAccount, Account beneficieryAccount) {
-		var transaction = new Transaction();
-		transaction.setSenderAccount(userAccount);
-		transaction.setReceiverAccount(beneficieryAccount);
-		transaction.setTransactionNumber(UUID.randomUUID().toString().replace("-", "").substring(12, 21));
-		transaction.setAmount(requestDetails.getAmount().doubleValue());
-		transaction.setCreatedTime(LocalDateTime.now());
-		transactionService.saveTransaction(transaction);
-	}
+  public void initiateAmountTransfer(TransferAccountDetailsDTO requestDetails, Account userAccount, Account beneficieryAccount) {
+    Long beneficieryBalance = beneficieryAccount.getBalance() + requestDetails.getAmount();
+    beneficieryAccount.setBalance(beneficieryBalance);
+    Long userBalance = userAccount.getBalance() - requestDetails.getAmount();
+    userAccount.setBalance(userBalance);
+    saveAccount(userAccount);
+  }
 
-	public void initiateAmountTransfer(TransferAccountDetailsDTO requestDetails, Account userAccount, Account beneficieryAccount) {
-		Long beneficieryBalance = beneficieryAccount.getBalance() + requestDetails.getAmount();
-		beneficieryAccount.setBalance(beneficieryBalance);
-		Long userBalance = userAccount.getBalance() - requestDetails.getAmount();
-		userAccount.setBalance(userBalance);
-		saveAccount(userAccount);
-	}
+  public synchronized void initiateTransfer(TransferAccountDetailsDTO transferAccountDetailsDTO, Account userAccount, Account beneficieryAccount) {
+    // if beneficiaryAccount is in list of beneficiaries then execute transfer..
+    for (Beneficiery beneficiery : userAccount.getBeneficiaryAccounts()) {
+      if (beneficiery.getBeneficieryAccountNumber().equals(beneficieryAccount.getAccountNumber())) {
+        initiateAmountTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
+        postAmountTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
+      }
+    }
+  }
 
-	public synchronized void initiateTransfer(TransferAccountDetailsDTO transferAccountDetailsDTO, Account userAccount, Account beneficieryAccount) {
-		// if beneficiaryAccount is in list of beneficiaries then execute transfer..
-		for (Beneficiery beneficiery : userAccount.getBeneficiaryAccounts()) {
-			if (beneficiery.getBeneficieryAccountNumber().equals(beneficieryAccount.getAccountNumber())) {
-				initiateAmountTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
-				postAmountTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
-			}
-		}
-	}
+  public boolean isValidRequest(TransferAccountDetailsDTO transferAccountDetailsDTO) {
+    return (null != transferAccountDetailsDTO && transferAccountDetailsDTO.getUserAccountNumber() != null && transferAccountDetailsDTO.getBeneficieryAccountNumber() != null
+        && transferAccountDetailsDTO.getAmount() > 0);
+  }
 
-	public boolean isValidRequest(TransferAccountDetailsDTO transferAccountDetailsDTO) {
-		return (null != transferAccountDetailsDTO && transferAccountDetailsDTO.getUserAccountNumber() != null && transferAccountDetailsDTO.getBeneficieryAccountNumber() != null
-				&& transferAccountDetailsDTO.getAmount() > 0);
-	}
+  public ResponseMessage fundTransfer(TransferAccountDetailsDTO transferAccountDetailsDTO) {
 
-	public ResponseMessage fundTransfer(TransferAccountDetailsDTO transferAccountDetailsDTO) {
+    // validate request
+    if (!isValidRequest(transferAccountDetailsDTO)) {
 
-		// validate request
-		if (!isValidRequest(transferAccountDetailsDTO)) {
+      return new ResponseMessage("Check Request Details");
+    }
 
-			return new ResponseMessage("Check Request Details");
-		}
+    var userAccount = getByAccount(transferAccountDetailsDTO.getUserAccountNumber());
+    var beneficieryAccount = getByAccount(transferAccountDetailsDTO.getBeneficieryAccountNumber());
 
-		var userAccount = getByAccount(transferAccountDetailsDTO.getUserAccountNumber());
-		var beneficieryAccount = getByAccount(transferAccountDetailsDTO.getBeneficieryAccountNumber());
+    // check Accounts are valid and have balance..
+    if (null != userAccount && null != beneficieryAccount) {
 
-		//check Accounts are valid and have balance..
-		if (null != userAccount && null != beneficieryAccount) {
+      if (userAccount.getBalance() <= 0 && (userAccount.getBalance() < transferAccountDetailsDTO.getAmount())) {
+        return new ResponseMessage("Balance Insufficient - Please add Amount");
+      }
 
-			if (userAccount.getBalance() <= 0 && (userAccount.getBalance() < transferAccountDetailsDTO.getAmount())) {
-				return new ResponseMessage("Balance Insufficient - Please add Amount");
-			}
-
-			// initiate transfer..
-			if (userAccount.getBeneficiaryAccounts() != null && !userAccount.getBeneficiaryAccounts().isEmpty()) {
-				initiateTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
-				return new ResponseMessage("Amount Transferred");
-			}
-
-		}
-		return new ResponseMessage("Enter Correct Account numbers to process");
-	}
-
+      // initiate transfer..
+      if (userAccount.getBeneficiaryAccounts() != null && !userAccount.getBeneficiaryAccounts().isEmpty()) {
+        initiateTransfer(transferAccountDetailsDTO, userAccount, beneficieryAccount);
+        return new ResponseMessage("Amount Transferred");
+      }
+    }
+    return new ResponseMessage("Enter Correct Account numbers to process");
+  }
 }
